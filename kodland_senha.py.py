2fc403f43578e23cde8f5c7856007d3b6238efb7
GabrielSchubertNordt <gabrielnordt190711@gmail.com>
2026-03-15 16:03:03 -0300

    Versão atualizada para VS CODE

--- kodland_senha.py.py
+++ kodland_senha.py.py
@@ -4,9 +4,9 @@ caracteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234
 
 tamanho = int(input("Digite o comprimento da senha: "))
 
-senha = ""   # variável onde a senha será armazenada
+senha = ""  # variável onde a senha será armazenada
 
-for_in_range(tamanho):
+for _ in range(tamanho):
     senha += random.choice(caracteres)
 
 print("Senha gerada:", senha)
